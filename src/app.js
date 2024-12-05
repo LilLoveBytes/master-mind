4a8870f85eadd1536aa160e8b7f24d8b987daa71
La Tasha Pollard <latashajpollard@gmail.com>
2024-12-05 17:06:39 -0600

    basic features and basic ui

--- src/app.js
+++ src/app.js
@@ -7,11 +7,14 @@ const cors = require("cors"); // for communication between front and back end
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'ejs')
+app.set('views', './src/views');
+
 app.use(cors()); // middleware to enable CORS 
 app.use(express.json()); // middleware to parse JSON
 
 app.get("/", (req, res) => {
-  res.send("Welcome to the Mastermind API");
+  res.render("mastermind" , {title: "Mastermind"});
 }); //middleware to get the root route
 
 app.use("/game", gameRoute); // middleware to use the gameRoute
--- src/controllers/gameController.js
+++ src/controllers/gameController.js
@@ -4,11 +4,13 @@ const random = require("../services/randomAPIService");
 
 let secretCombo = [];
 let attempts = 0;
+let guesses = [];
 
 exports.startGame = async (req, res) => {
 	try {
 		secretCombo = await random.generateSecretCombo();
 		attempts = 0;
+    guesses = [];
 		console.log("Secret combo:", secretCombo);
 		res
 			.status(200)
@@ -18,18 +20,20 @@ exports.startGame = async (req, res) => {
 	}
 };
 
-exports.submitGuess = (req, res) => {
+exports.submitGuess = async (req, res) => {
 	try {
 		const { guess } = req.body;
+		if (secretCombo.length === 0) {
+			await this.startGame(req, res);
+		}
 		if (!guess || guess.length !== 4) {
 			return res
 				.status(400)
 				.json({ message: "Guess must be exactly 4 numbers long" });
 		}
-		if (secretCombo.length === 0) {
-			return res.status(400).json({ message: "Game has not started" });
-		}
 		attempts++;
+		guesses.push(guess);
+
 		if (attempts > 10) {
 			return res
 				.status(400)
@@ -38,8 +42,9 @@ exports.submitGuess = (req, res) => {
 		const guessArray = guess.split("");
 		console.log("Guess array:", guessArray);
 
-		const feedback = random.giveFeedback(guessArray, secretCombo);
-		res.status(200).json({ message: "Guess submitted", feedback });
+		const feedback = this.giveFeedback(guessArray, secretCombo);
+		const gameHistory = await this.getGameStatus(req, res);
+		res.status(200).json({ message: "Guess submitted", feedback, gameHistory });
 	} catch (error) {
 		console.error(error);
 		res.status(500).json({ message: "Error submitting guess", error });
@@ -48,9 +53,52 @@ exports.submitGuess = (req, res) => {
 
 exports.getGameStatus = (req, res) => {
 	try {
-		const status = random.getGameStatus();
-		res.status(200).json({ message: "Game status", status });
+		if (secretCombo.length === 0) {
+			return res.status(200).json({ message: "Game has not started" });
+		}
+
+		let attemptsMade = attempts;
+		let attemptsRemaining = 10 - attempts;
+
+		return  `${attemptsMade} guess(es) made so far. ${attemptsRemaining} attempts remaining. \nGuesses: ${guesses}`;  
+
 	} catch (error) {
-		res.status(500).json({ message: "Error getting game status", error });
+		// res.status(500).json({ message: "Error getting game status", error });
+		console.error(error);
+	}
+};
+
+exports.giveFeedback = (guessArray, secretCombo) => {
+	// initialize counts
+	let correctNumbers = 0;
+	let exactMatchces = 0;
+
+	// compare guess to secret combo
+	// if guess matches, return "You win!"
+	if (guessArray === secretCombo) {
+		return "You win!";
+	} else {
+		// if guess doesn't match, return conditional feedback
+
+		//check for correct numbers (any position)
+		for (let i = 0; i < guessArray.length; i++) {
+			if (secretCombo.includes(guessArray[i])) {
+				correctNumbers++;
+			}
+		}
+		// check for correct positions (exact matches)
+		for (let i = 0; i < guessArray.length; i++) {
+			if (guessArray[i] === secretCombo[i]) {
+				exactMatchces++;
+			}
+
+			return (
+				correctNumbers +
+				" correct number(s) " +
+				"with " +
+				exactMatchces +
+				" in the correct position"
+			);
+		}
 	}
 };
